56240a1db72ef4387319e3e59efe82acd9b7decb
Mr Hacker <charhankinimbom828@gmail.com>
2025-03-19 09:01:24 -0700

    creating a package with a module for operations

--- APP.py
+++ APP.py
@@ -1,6 +1,27 @@
-print("Hello World!!")
+from package import operation
+
+print("Hello")
 num1 = int(input("enter a number: "))
 num2 = int(input("enter a number: "))
 
-result = num1 + num2
-print(result)
+print("What Dod U Wanna Do Today:\n\
+    1. Addition\n\
+    2. Multiplication\
+")
+
+operations = input("ENTER THE OPERATION YOU WILL LIKE TO USE: ")
+
+if len(operations) == 0:
+    print("Enter a valid operation")
+else:
+    addition = "1"
+    multiply = "2"
+
+    operations = operations.lower()
+    if operations == addition :
+        result = operation.add(num1,num2)
+        print(result)
+
+    elif operations == multiply:      
+        result = operation.multi(num1,num2)
+        print(result)
---
+++ package/operation.py
@@ -0,0 +1,8 @@
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+def multi(num1, num2):
+    result = num1 * num2
+    return result
